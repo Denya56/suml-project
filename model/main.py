c2d4008109dbeb46b864bbe4c2461477c9d40ba7
Antoni Malinowski <antonimalinowski.kontakt@gmail.com>
2023-12-18 09:49:07 +0100

    feat(model): Connect model with frontend - App is working

--- model/main.py
+++ model/main.py
@@ -3,9 +3,9 @@ import pandas as pd
 import spacy
 import pickle
 from wordcloud import WordCloud
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
 from tqdm import tqdm
 import re
 import seaborn as sns
@@ -14,7 +14,7 @@ max_words = 5000
 max_len = 500
 
 # loading the saved model
-loaded_model = load_model('D:/Uni/7th sem/suml-project/model/modelv2.h5')
+loaded_model = load_model('model\modelv2.h5')
 
 nlp = spacy.load('en_core_web_md', disable=['ner', 'parser'])
 nlp.add_pipe('sentencizer')
@@ -31,12 +31,35 @@ def lemmatize(text):
     return ' '.join([x.lemma_ for x in nlp(text)])
     
 def main():
-    
+    st.markdown("""
+    <style>
+    .block-container {
+        background-color: #000000;
+        color: #00ff00;
+        font-family: Courier New;
+    }
+    .st-emotion-cache-10trblm {
+        color: #00ff00;
+        text-align: center;
+    }
+    .stTextArea textarea {
+        background-color: #333333;
+        color: #00ff00;
+    }
+    .stButton>button {
+        color: #00ff00;
+        background-color: #333333;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
     tokenizer = Tokenizer(num_words=max_words)
     
-    st.title('Diabetes Prediction Web App')
-    user_input = st.text_input("Enter a review (or 'exit' to end): ")
-    user_input = remove_stopwords(user_input)
+    st.title('Review Sentiment Analysis')
+    with st.form(key='game_review_form'):
+        user_input = st.text_input("Enter a review (or 'exit' to end): ")
+        user_input = remove_stopwords(user_input)
+        submit_button = st.form_submit_button(label='Submit')
     
     user_input = lemmatize(user_input)
     print(user_input)
@@ -56,6 +79,11 @@ def main():
         predicted_label = 0
     
     print("Predicted Label:", predicted_label)
+    if submit_button:
+        if predicted_label == 1:
+            st.write(f"Predicted label: Positive")
+        elif predicted_label == 0:
+            st.write(f"Predicted label: Negative")
 
 
 if __name__ == '__main__':
